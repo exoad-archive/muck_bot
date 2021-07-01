da5c18753e0a33c3184f4564138e7f3a5f6eded4
JackMeng <error666jm@gmail.com>
2021-07-01 10:55:52 -0400

    refactor

--- src/main/app.js
+++ src/main/app.js
@@ -2,7 +2,7 @@ const { Discord, Client } = require('discord.js');
 const colors = require('colors');
 const read = require('read-file');
 const bot = new Client();
-const utili = require('/json/utili.json')
+const utili = require('src/json/utili.json')
 
 // TODO: Add token
 bot.login(read('keys/token.lock', {encoding: 'utf8'}));
