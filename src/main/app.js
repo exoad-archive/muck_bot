16a93b959b08c4a8e3dfa8d914099f9075f88aee
JackMeng <error666jm@gmail.com>
2021-07-01 10:22:15 -0400

    updated request handler

--- src/main/app.js
+++ src/main/app.js
@@ -1,1 +1,12 @@
-console.log("hi");
+import { Client } from "discord.js"
+import Discord from Discord.js
+
+const colors = require('colors');
+const read = require('read-file');
+const bot = new Client();
+
+bot.on('ready', async () => {
+    console.info("Online.".bold.blue);
+})
+
+bot.login(read('keys/token.lock', {encoding: 'utf8'}));
