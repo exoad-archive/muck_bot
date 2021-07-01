f6275824dbdd89fcc048eaadddcedc1bc4ce2cea
JackMeng <error666jm@gmail.com>
2021-07-01 10:25:52 -0400

    update todo

--- src/main/app.js
+++ src/main/app.js
@@ -4,9 +4,11 @@ import Discord from Discord.js
 const colors = require('colors');
 const read = require('read-file');
 const bot = new Client();
+const utili = require('/json')
 
 bot.on('ready', async () => {
     console.info("Online.".bold.blue);
 })
 
+// TODO: Add token
 bot.login(read('keys/token.lock', {encoding: 'utf8'}));
